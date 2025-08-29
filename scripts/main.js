14692b1a0b6972931a89af61edd61916a8e4872b
Anthony <olsenan@gmail.com>
2025-08-29 00:53:52 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 // App entry (module)
-import { initGame, startGame, startEndless, togglePause, resumeGame, quitToTitle } from './game.js';
+import {
+  initGame, startGame, startEndless, togglePause,
+  resumeGame, quitToTitle,
+  pressLeft, releaseLeft, pressRight, releaseRight, pressAttack, releaseAttack
+} from './game.js';
 import { bindUI, showTitle } from './ui.js';
+import { attachTouch } from './touch.js';
 
 async function boot(){
   bindUI({
@@ -9,6 +14,12 @@ async function boot(){
     onResume:  ()=> resumeGame(),
     onQuit:    ()=> quitToTitle(),
   });
+
+  // On-screen touch controls for mobile
+  attachTouch({
+    pressLeft, releaseLeft, pressRight, releaseRight, pressAttack, releaseAttack, togglePause
+  });
+
   showTitle();
   await initGame();
 }
